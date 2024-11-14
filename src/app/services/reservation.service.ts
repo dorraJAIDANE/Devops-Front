36831761b4396448ea7fbfa24a7062c39c156d14
dorraJAIDANE <113977782+dorraJAIDANE@users.noreply.github.com>
2024-11-14 22:54:36 +0100

    Update reservation.service.ts

--- src/app/services/reservation.service.ts
+++ src/app/services/reservation.service.ts
@@ -7,7 +7,7 @@ import { Reservation } from '../models/Reservation';
   providedIn: 'root'
 })
 export class ReservationService {
-  private baseUrl = 'http://spring-app:8282/api/reservations'; // Remplacez par l'URL de votre API
+  private baseUrl = 'http://spring-app:8082/api/reservations'; // Remplacez par l'URL de votre API
 
   constructor(private http: HttpClient) {}
 
